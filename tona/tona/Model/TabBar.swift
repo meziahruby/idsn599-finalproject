3d3d77afebc227fcb08d1c65e331fcbbc456b889
nakudino <112789966+nakudino@users.noreply.github.com>
2023-04-10 21:03:49 -0700

    Started Swift project

---
+++ tona/tona/Model/TabBar.swift
@@ -0,0 +1,8 @@
+//
+//  TabBar.swift
+//  tona
+//
+//  Created by Njideka Akudinobi on 4/17/23.
+//
+
+import Foundation
---
+++ tona/tona/tonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tonaApp.swift
+//  tona
+//
+//  Created by Meziah Cristobal on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
