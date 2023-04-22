06ff7ce5140fa552d522a125e19c1e7af50c1821
Meziah Cristobal <meziahcristobal@gmail.com>
2023-04-22 12:13:56 -0700

    Resolve ContentView

--- tona/tona/View/ContentView.swift
+++ tona/tona/View/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = ItineraryViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +18,8 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        
+        Itinerary().environmentObject(viewModel)
     }
 }
 
