24e848568b12f20db9d1788981ea0228e11caf21
nakudino <112789966+nakudino@users.noreply.github.com>
2023-04-23 19:02:57 -0700

    4.23 - add button for find view

--- tona/tona/Helper/RectangleImage.swift
+++ tona/tona/Helper/RectangleImage.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct RectangleImage: View {
     
+    @EnvironmentObject var viewModel: FindViewModel
+    @State private var isExpanded = false
+    
     var image: Image
     var aspect: CGFloat = 1.0
     
@@ -17,16 +20,33 @@ struct RectangleImage: View {
     }
     
     var body: some View {
-        Rectangle()
-            .aspectRatio(aspect, contentMode: .fit)
-            .overlay {
-                image
-                    .resizable()
-                    .scaledToFill()
+                Rectangle()
+                    .aspectRatio(aspect, contentMode: .fit)
+                    .overlay {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    }
+                    .overlay(
+                       Button(action: {
+                           // Perform action to add image to another view
+                           print("Add image to another view")
+                       }, label: {
+                           Image(systemName: "plus.circle.fill")
+                               .font(.system(size: 16))
+                               .foregroundColor(.white)
+                               .padding(5)
+                               .background(Color(red: 0.922, green: 0.608, blue: 0.329))
+                               .clipShape(Circle())
+                               .opacity(0.8)
+                       })
+                       .padding(10)
+                       .opacity(0.8),
+                       alignment: .bottomTrailing
+                   )
+                    .clipped()
             }
-            .clipped()
-    }
-}
+        }
 
 struct RectangleImage_Previews: PreviewProvider {
     static var previews: some View {
--- tona/tona/ViewModel/FindViewModel.swift
+++ tona/tona/ViewModel/FindViewModel.swift
@@ -9,29 +9,20 @@
 import SwiftUI
 
 class FindViewModel: ObservableObject {
-
+    
     @Published var findPic: FindImage = FindImage()
     @Published var searchText: String = ""
-
+ 
     var findAll: [Find] {
         findPic.findData
     }
-        
+    
     var filteredImages: [Find] {
-            if searchText.isEmpty {
-                return findAll
-            } else {
-                return findAll.filter { $0.location.localizedCaseInsensitiveContains(searchText)}
-            }
+        if searchText.isEmpty {
+            return findAll
+        } else {
+            return findAll.filter { $0.location.localizedCaseInsensitiveContains(searchText)}
         }
     }
-
-/*   var filteredImages: [ImageModel] {
- if searchText.isEmpty {
- return images
- } else {
- return images.filter { $0.imageName.lowercased().contains(searchText.lowercased()) }
- }
- }
- }
- */
+    
+}
