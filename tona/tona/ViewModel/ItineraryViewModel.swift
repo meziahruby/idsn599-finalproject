0418e3727c4e116b35e982ca20caa80230f6fcce
username <janae.michele@gmail.com>
2023-04-28 14:36:15 -0700

    4/28 1:1 fixes

--- tona/tona/ViewModel/ItineraryViewModel.swift
+++ tona/tona/ViewModel/ItineraryViewModel.swift
@@ -10,13 +10,10 @@ import SwiftUI
 class ItineraryViewModel: ObservableObject {
 
     @Published private var model: ItineraryData = ItineraryData()
-<<<<<<< HEAD
-=======
     
     var itinerariesData: [ItineraryModel] {
         model.itineraries
     }
->>>>>>> dd8cf079bddbbdedbbac383a08c9331b050a90d9
 
     var events: [EventModel] {
         model.parisEvents
